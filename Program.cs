a8fa219b05d35185f8f08ac63e716e8eec9ad068
Brandon Tosch <Brandon.Tosch@veritas.com>
2017-09-14 14:41:54 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tfstate_manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
